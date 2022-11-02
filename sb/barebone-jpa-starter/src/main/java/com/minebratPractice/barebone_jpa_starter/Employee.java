4e0d0f77a3a4148270009f8c0867a47ac6d9757e
Bugs Bunny <zzzz@gmail.com>
2022-11-02 15:49:31 +0530

    enum - string storage

--- sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/Employee.java
+++ sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/Employee.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -28,7 +30,7 @@ public class Employee {
 	@Temporal(TemporalType.TIME)
 	private Date dob;
 	
-	
+	@Enumerated(EnumType.STRING)
 	private EmployeeType type; // mapping enum with member variable 
 	
 	public EmployeeType getType() {
--- sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/EmployeeType.java
+++ sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/EmployeeType.java
@@ -1,6 +1,7 @@
 package com.minebratPractice.barebone_jpa_starter;
 
 public enum EmployeeType {
+	freelancer,  
 FULL_TIME,
 CONTRACT
 }
