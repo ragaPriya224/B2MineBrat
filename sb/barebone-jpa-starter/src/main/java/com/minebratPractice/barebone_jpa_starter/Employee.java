a0b1946294aea749f6f10fa828bdbfc4301f4651
Bugs Bunny <zzzz@gmail.com>
2022-11-02 15:32:34 +0530

    datatype-temporal time

--- sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/Employee.java
+++ sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/Employee.java
@@ -25,7 +25,7 @@ public class Employee {
 	@Column(unique = true, length = 10, nullable = true, updatable=false)
 	private String ssn;
 	
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.TIME)
 	private Date dob;
 	
 	public Date getDob() {
