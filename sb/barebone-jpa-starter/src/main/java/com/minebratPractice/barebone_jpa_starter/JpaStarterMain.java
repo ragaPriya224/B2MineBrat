1261ffc2a5f8b5f69ac2c6bac0383213dbd78eee
Bugs Bunny <zzzz@gmail.com>
2022-11-03 15:53:04 +0530

    second entity - one to one relationship

--- sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/JpaStarterMain.java
+++ sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/JpaStarterMain.java
@@ -2,6 +2,7 @@ package com.minebratPractice.barebone_jpa_starter;
 
 import java.util.Date;
 
+import javax.persistence.Access;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,9 +18,20 @@ public class JpaStarterMain {
 
 		EntityManager em = emf.createEntityManager();
 
-		Employee employee = em.find(Employee.class, 1);
+//		Employee employee = em.find(Employee.class, 1);
 		
-
+		
+		AccessCard  card1 = new AccessCard();
+		card1.setFirmwareVersion("1.0.0");
+		card1.setIssuedDate(new Date());
+		card1.setActive(true);
+//		card1.setId(110);
+		AccessCard  card2 = new AccessCard();
+		card2.setFirmwareVersion("2.0.0");
+		card2.setIssuedDate(new Date());
+		card2.setActive(false);
+//		card2.setId(20);
+		
 //		System.out.println(employee);
 //		Employee e2 = em.find(Employee.class, 2);
 //
@@ -29,25 +41,30 @@ public class JpaStarterMain {
 //		System.out.println("output for id3 is " + e3); //in sql, o/p is empty result, in java, it's null
 		//		
 		//		//create instance of employee
-		//		Employee e = new Employee();
-		////		e.setId(1);
-		//		e.setName("hello");
-		//		e.setSsn("123");
-		//		e.setDob(new Date());
-		//		e.setType(EmployeeType.FULL_TIME); // value will be o.bcoz integer type
-		//		
-		//		Employee e2 = new Employee();
-		////		e2.setId(2);
-		//		e2.setName("afternoon");
-		//		e2.setSsn("456");
+				Employee e = new Employee();
+		//		e.setId(1);
+				e.setName("hello");
+				e.setSsn("123");
+				e.setDob(new Date());
+				e.setType(EmployeeType.FULL_TIME); // value will be o.bcoz integer type
+e.setCard(card1);
+			
+				Employee e2 = new Employee();
+		//		e2.setId(2);
+				e2.setName("afternoon");
+				e2.setSsn("456");
+				e2.setCard(card2);// not linking individual value, linking the instance
 		//		
 		//
 		//		
 				EntityTransaction t = em.getTransaction();
 				t.begin(); //start transaction
-//				em.persist(employee); //save my employee data 
-				em.remove(employee);
-		//		em.persist(e2);
+				em.persist(e); //save my employee data 
+				em.persist(e2);
+				em.persist(card1);
+				em.persist(card2);
+//				em.remove(employee);
+		//		
 		//
 				t.commit();/// it will get saved 
 		em.close();
