2b85025f37d32096c2c11401859332981dd6c097
Bugs Bunny <zzzz@gmail.com>
2022-11-03 15:22:31 +0530

    creation of second entity

---
+++ sb/barebone-jpa-starter/src/main/java/com/minebratPractice/barebone_jpa_starter/AccessCard.java
@@ -0,0 +1,48 @@
+package com.minebratPractice.barebone_jpa_starter;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//1 employee - > 1 access card
+
+@Entity
+public class AccessCard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private Date issuedDate;
+	private boolean isActive;
+	private String firmwareVersion;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getIssuedDate() {
+		return issuedDate;
+	}
+	public void setIssuedDate(Date issuedDate) {
+		this.issuedDate = issuedDate;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public String getFirmwareVersion() {
+		return firmwareVersion;
+	}
+	public void setFirmwareVersion(String firmwareVersion) {
+		this.firmwareVersion = firmwareVersion;
+	}
+	
+	
+}
