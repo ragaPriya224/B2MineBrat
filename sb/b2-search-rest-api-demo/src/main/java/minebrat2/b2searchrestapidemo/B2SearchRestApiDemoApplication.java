ebbe6c6ae442a8f92ded6311565810df49a573df
Bugs Bunny <zzzz@gmail.com>
2022-11-04 16:56:28 +0530

    initial bare project

---
+++ sb/b2-search-rest-api-demo/src/main/java/minebrat2/b2searchrestapidemo/B2SearchRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package minebrat2.b2searchrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2SearchRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2SearchRestApiDemoApplication.class, args);
+	}
+
+}
---
+++ sb/b2-search-rest-api-demo/src/test/java/minebrat2/b2searchrestapidemo/B2SearchRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package minebrat2.b2searchrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2SearchRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
